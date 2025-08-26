89e5bf2155b44dad26660b0a92c2241147e7e7fa
gari-app <herrerotomas23@gmail.com>
2025-08-26 13:57:02 -0300

    Create supabase.ts

---
+++ lib/supabase.ts
@@ -0,0 +1,17 @@
+import { createBrowserClient, createServerClient } from '@supabase/ssr'
+import { cookies } from 'next/headers'
+
+export const createClient = () => {
+  const cookieStore = cookies()
+  return createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    { cookies: { get: (name: string) => cookieStore.get(name)?.value } }
+  )
+}
+
+export const createClientBrowser = () =>
+  createBrowserClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  )
