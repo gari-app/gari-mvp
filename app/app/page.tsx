4cb06e3c5c9b9d2f6654cb92f354ff4e7d155c70
gari-app <herrerotomas23@gmail.com>
2025-09-02 13:10:07 -0300

    Update page.tsx

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -1,12 +1,17 @@
+import HeaderApp from './header'
+
 export default function AppHome() {
-return (
-<main style={{padding:24}}>
-<h2>¿Qué querés hacer?</h2>
-<ul>
-<li><a href="/app/search">Buscar cocheras (Conductor)</a></li>
-<li><a href="/app/publicar">Publicar cochera (Anfitrión)</a></li>
-<li><a href="/app/reservas">Mis reservas</a></li>
-</ul>
-</main>
-)
+  return (
+    <>
+      <HeaderApp />
+      <main style={{padding:24}}>
+        <h2>¿Qué querés hacer?</h2>
+        <ul style={{lineHeight:1.8}}>
+          <li><a href="/app/search">Buscar cocheras (Conductor)</a></li>
+          <li><a href="/app/publicar">Publicar cochera (Anfitrión)</a></li>
+          <li><a href="/app/reservas">Mis reservas</a></li>
+        </ul>
+      </main>
+    </>
+  )
 }
